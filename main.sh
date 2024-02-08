b71d27042ea5f9a61080f886d90446d4abaaaa82
avidalto <avidalto@hawk.iit.edu>
2024-02-08 12:52:51 -0600

    Add job submission

--- main.sh
+++ main.sh
@@ -19,6 +19,7 @@ fi
 
 if ! [ -f "resources/host/inputs.sh" ]; then
     echo "ERROR - Missing file ./resources/host/inputs.sh - Resource wrapper failed"
+    exit 1
 fi
 
 source resources/host/inputs.sh
@@ -29,3 +30,16 @@ cat resources/host/inputs.sh >> benchmarks/${benchmark}/batch.sh
 cat benchmarks/${benchmark}/main.sh >> benchmarks/${benchmark}/batch.sh
 
 rsync -avzq -e 'ssh -o StrictHostKeyChecking=no' --rsync-path="mkdir -p ${resource_jobdir}/benchmarks/ && rsync" benchmarks/${benchmark} ${resource_publicIp}:${resource_jobdir}/benchmarks/
+
+echo; echo; echo RUNNING JOB SCRIPT ${resource_publicIp}:${resource_jobdir}/benchmarks/${benchmark}/batch.sh
+# Submit job and get job id
+if [[ ${jobschedulertype} == "SLURM" ]]; then
+    jobid=$($sshcmd ${submit_cmd} ${resource_jobdir}/benchmarks/${benchmark}/batch.sh | tail -1 | awk -F ' ' '{print $4}')
+elif [[ ${jobschedulertype} == "PBS" ]]; then
+    jobid=$($sshcmd ${submit_cmd} ${resource_jobdir}/benchmarks/${benchmark}/batch.sh)
+fi
+
+if [[ "${jobid}" == "" ]];then
+    echo "ERROR submitting job - exiting the workflow"
+    exit 1
+fi
